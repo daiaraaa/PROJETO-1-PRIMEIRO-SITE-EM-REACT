cbce2ece3d525e817cecf679a9447bee55507708
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-10-18 08:47:12 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+export default function App(){
+  return(
+    <div className="App">
+      <img src="../../imagem.png"/>
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usúarios web e nativas</p>
+      <button>Aprenda React</button>
+      <button>Referências de API</button>
+      <hr></hr>
+      <h2>Crie interfaces de usúarios de componentes</h2>
+      <p>React permite que você construa interfaces de usúarios a partir de pedaços individuais chamados componentes</p>
+      <hr></hr>
+      <h2>Escreva componentes com um código e marcação</h2>
+      <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React</p>
+      <hr></hr>
+      <ul>
+      <h2>Próximos passos</h2>
+      <li>Uso de dados dinâmicos no JSX</li>
+      <li>Criação de novos componentes</li>
+      <li>Estilização de componentes</li>
+      <li>Reutilização de componentes</li>
+      <li>Uso de props e children</li>
+      <li>Uso de eventos no JavaScript</li>
+      </ul>
+    </div>
+  )
+}
